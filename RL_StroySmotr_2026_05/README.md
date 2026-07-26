a0ff69d9f1c76741c0496663fa08b17a684d6ebb
RollingHog <46373241+RollingHog@users.noreply.github.com>
2026-07-26 04:57:08 +0300

    result done

--- RL_StroySmotr_2026_05/README.md
+++ RL_StroySmotr_2026_05/README.md
@@ -1,6 +1,8 @@
 It expects SQL engine duckdb.exe to be extracted in this folder
 Node.js should be installed in the system
 
+Основной мастерский документ:
+https://docs.google.com/document/d/1CYvge_PyT1tNirb_AUUlP45kkgWX-TEkvmAYIhsbgB4/edit?tab=t.isg316zi2hon
 Форма и таблица результатов:
 https://docs.google.com/forms/d/1h5n5o2B7RMB2jUoCbcQ8qi9owvr_Seu7rP9DE2zTmZ0/edit#responses
 https://docs.google.com/spreadsheets/d/1lmT6mRzulvbflvRmDkPHj1qmX_jH_hxFQleXbyIeHNU/edit?gid=790754062#gid=790754062
--- RL_StroySmotr_2026_05/g_further.sql
+++ RL_StroySmotr_2026_05/g_further.sql
@@ -16,6 +16,21 @@ INSERT INTO bad_digit_names (digit, name) VALUES
       (9, 'Мало монстры или Боевикам не с кем воевать'),
       (10, 'Я не понял что делать');
 
+
+CREATE TABLE improve_digit_names (
+    digit INTEGER PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+INSERT INTO improve_digit_names (digit, name) VALUES
+    (1, 'Азартные игры' ),
+    (2, 'Арена для боёв один на один'),
+    (3, 'Построение в начале игры с повтором основных правил'),
+    (4, 'Больше квестов чисто для заработка, без привязки к фракции'),
+    (5, 'Игровая возможность "закрыть" здание на замок (напр. чтобы живая стража сходила пообедать)'),
+    (6, 'Проработка законов, возможно суд'),
+    (7, 'Облегчить поиск квестодателей'),
+    (8, '"Серийный" (сквозной, связанный) сюжет на несколько игр');
+
 -- -- summary for all experiences
 -- WITH all_data AS (
 --     SELECT 
@@ -24,11 +39,8 @@ INSERT INTO bad_digit_names (digit, name) VALUES
 --     FROM default_excluded
 -- )
 -- SELECT 
---     -- a.experience,
 --     dn.name as digit_name,
---     -- a.digit,
 --     COUNT(*) as count,
---     -- ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (PARTITION BY a.experience), 2) as percentage_in_field
 -- FROM all_data a
 -- JOIN bad_digit_names dn ON a.digit = dn.digit
 -- GROUP BY dn.name, a.digit
